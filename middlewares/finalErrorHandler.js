02d0b4c6f5635aa4d5084f1121ec89ea859e731b
Andrey Rastegaev <embrion83@yandex.ru>
2021-02-20 22:44:41 +0300

    fix final error

--- middlewares/finalErrorHandler.js
+++ middlewares/finalErrorHandler.js
@@ -1,20 +1,19 @@
-// const { SERVER_ERR } = require('../utils/errorMessages');
+const { SERVER_ERR } = require('../utils/errorMessages');
 
 const finalErrorHandler = (err, req, res, next) => {
-  if (err.status) {
-    res.status(err.status).send(err.message);
-    return;
-  }
-  res.status(500).send(err.message);
+  // if (err.status) {
+  //   res.status(err.status).send(err.message);
+  //   return;
+  // }
   // res.status(500).send({ message: err.message });
-  // const { statusCode = 500, message } = err;
-  // res
-  //   .status(statusCode)
-  //   .send({
-  //     message: statusCode === 500
-  //       ? SERVER_ERR
-  //       : message,
-  //   });
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? SERVER_ERR
+        : message,
+    });
   next();
 };
 
